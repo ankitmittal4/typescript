78cbb57f97a0f14af31a13570de56f80413cecf5
Ankit <ankitmittal0814@gmail.com>
2025-08-27 22:05:23 +0530

    Create function file in ts

---
+++ function.js
@@ -0,0 +1,6 @@
+function signup(name, email, password, isPremium) {
+    if (isPremium === void 0) { isPremium = false; }
+    console.log(name, email, password, isPremium);
+    return;
+}
+signup("Ankit", "ankit@gmail.com", 123);
---
+++ function.ts
@@ -0,0 +1,6 @@
+function signup(name: string, email: string, password: number, isPremium: boolean = false) {
+    console.log(name, email, password, isPremium);
+    return;
+}
+
+signup("Ankit", "ankit@gmail.com", 123);
