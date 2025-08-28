2ecb4815a246c51d383cb3c8bc54716b1be7362f
Ankit <ankitmittal0814@gmail.com>
2025-08-28 08:23:18 +0530

    Create tuple file with examples

---
+++ tuple.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var value;
+value = [1, "Ankit", true];
+value = [101, "Ankit Mittal", false];
+console.log(value);
---
+++ tuple.ts
@@ -0,0 +1,7 @@
+let value: [number, string, boolean];
+
+value = [1, "Ankit", true];
+value = [101, "Ankit Mittal", false];
+console.log(value);
+
+export { }
